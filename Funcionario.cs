ef85489cf9b18936d52bcbaf3a26f0d5bd814baa
Eduardo Marçal Clemente <80399639+Marshal86@users.noreply.github.com>
2022-06-27 15:42:47 -0300

    Add files via upload
    Utilizando listas em c# , utilizando orientação a objeto com a classe Funcionarios, passando com dados Id, Nome e Salario , mas por enquanto sem nenhum
    tratamento dos dados.

---
+++ Funcionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Exlist
+{
+    class Funcionario
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public double _salario { get;  private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            _salario = salario;
+        }
+
+        public void Salario (double porcentagem)
+        {
+            _salario =_salario + _salario * porcentagem / 100.0;
+
+        }
+
+        public override string ToString()
+        {
+            return Id
+                + ", "
+                + Nome
+                + ", "
+                + _salario.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Exlist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+
+           
+            
+            
+            Console.WriteLine("How many employees will be registered");
+            int n = int.Parse(Console.ReadLine());
+
+            List<Funcionario> ListaEmp = new List<Funcionario>();
+
+            for (int i = 0; i<n; i++)
+            {
+                Console.Write("Emplyoee #{0}", i);
+                Console.WriteLine("Id : ");
+                int id = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+                Console.Write("Nome : ");
+                string nome = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Salario : ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.WriteLine();
+
+                ListaEmp.Add(new Funcionario(id, nome, salario));
+
+              
+            }
+
+            Console.Write("Enter the employee id that will have salary increase :");
+            int achaID = int.Parse(Console.ReadLine());
+
+            Funcionario func = ListaEmp.Find(x=> x.Id == achaID);
+
+            if (func != null)
+            {
+                Console.Write("Enter the percentage: ");
+                double porcento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                func.Salario(porcento);
+            } else
+            {
+                Console.WriteLine("This Id does not exist");
+            }
+       
+
+
+            foreach (Funcionario obj in ListaEmp)
+            {
+                Console.WriteLine(obj);
+            } 
+
+
+        }
+    }
+}
